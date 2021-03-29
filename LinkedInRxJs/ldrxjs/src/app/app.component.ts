86132eca87dacedf27a876bae880d79e48e45924
JoshSevy <joshuasevy@outlook.com>
2021-03-28 21:07:43 -0600

    Add Observable to project

---
+++ LinkedInRxJs/ldrxjs/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'ldrxjs';
+  observable$;
+
+  ngOnInit() {
+    const observable$ = Observable.create((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+    observable$.subscribe(
+      value => console.log(value),
+      err => {},
+      () => console.log('this is the end')
+    );
+
+  }
+
+  ngOnDestroy() {
+    this.observable$.unsubscribe();
+  }
+}
